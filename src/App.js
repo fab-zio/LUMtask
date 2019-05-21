71acf1e6bc2247b4eb764379a0654e744a3424ab
fab-zio <fabrizio.musa@yahoo.com>
2019-05-21 11:42:29 +0200

    4 point bullet checked

--- src/App.js
+++ src/App.js
@@ -1,11 +1,40 @@
-import React from 'react';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-    LUM
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      value: ""
+    };
+    this.enterText = this.enterText.bind(this);
+  }
+
+  enterText(e) {
+    e.preventDefault();
+    const text = e.target.value;
+    this.setState({
+      value: text
+    });
+    setInterval(() => {
+      this.setState({ value: "" });
+    }, 10000);
+  }
+  render() {
+    const { value } = this.state;
+    return (
+      <div className="App">
+        <h1>LUM</h1>
+        <textarea
+          value={this.state.value}
+          onChange={this.enterText}
+          rows="10"
+          placeholder="Type something in here..."
+        />
+        <div>{value}</div>
+      </div>
+    );
+  }
 }
 
 export default App;
